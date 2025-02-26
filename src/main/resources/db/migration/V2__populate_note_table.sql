d6732bda6ad632573eccab9cd8e2a9fae2524ddd
SspidersS <99414766+SspidersS@users.noreply.github.com>
2025-02-26 10:53:49 +0100

    d

--- src/main/resources/db/migration/V2__populate_note_table.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO note (title, content) VALUES ('First Note', '1');
-INSERT INTO note (title, content) VALUES ('Second Note', '2');
-INSERT INTO note (title, content) VALUES ('Third Note', '3');
